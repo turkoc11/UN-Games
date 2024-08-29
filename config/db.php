6ad81d2f6a2024620ca9f784ae9e9f76281377d6
denver <shishkalovd@gmail.com>
2024-08-29 12:26:29 +0300

    add config

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php 
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;port=54314;dbname=game',
+    'username'=>'root',
+    'password'=>'NHY67ujm',
+    'charset'=>'UTF8',
+//    'enableLogging' => true, // включить логирование
+//    'enableProfiling' => true, // включить профилирование
+];
